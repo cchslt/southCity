6e615596df3634ca9dcf081d7b6100c9e6227c78
chenchaohai <979839467@qq.com>
2016-03-19 23:15:05 +0800

    commit

---
+++ manager/src/main/com/stone/southcity/test/test.java
@@ -0,0 +1,10 @@
+package main.com.stone.southcity.test;
+
+/**
+ * Created by chenchen on 2016/3/19.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
