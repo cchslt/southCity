61839a50e6f07f3bac1e29c066d08b8e7bcac70d
chenchaohai <979839467@qq.com>
2016-03-19 22:59:19 +0800

    first commit

---
+++ manager/src/main/com.stone.southcity.patterns/Observer.java
@@ -0,0 +1,10 @@
+package main.com.stone.southcity.patterns;
+
+/**
+ * Created by chenchen on 2016/3/19.
+ */
+public class Observer {
+    public static void main(String[] args) {
+        System.out.println("π€≤Ï’ﬂ£°");
+    }
+}
