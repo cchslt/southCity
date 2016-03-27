cab2d8022bb3cc90a369fbb7c0ac564d1fb386b5
chenchaohai <979839467@qq.com>
2016-03-28 00:04:04 +0800

    gitignore

--- manager/target/test/WEB-INF/classes/com/stone/southcity/patterns/observer/Observer.java
+++
@@ -1,8 +0,0 @@
-package com.stone.southcity.patterns.observer;
-
-/**
- * Created by chenchen on 2016/3/20.
- */
-public interface Observer {
-    public void update(String context);
-}
--- manager/target/test/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-fdfdfdfffgfgfgfdfdfdf
-<a href="/hello/showpage">第一次</a>
-</body>
-</html>
